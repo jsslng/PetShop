c53b86d2e87bd388c0ec3c943d6ca29157ed6dba
Ícaro Raupp <budicaro@gmail.com>
2016-02-24 11:22:05 -0300

    criando a estrutura base de usuário

---
+++ main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+
+from usuario import test_usuario
+
+
+test_usuario()
---
+++ usuario.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+
+from animal import Animal
+
+
+class Usuario:
+    def __init__(self, nome, email, telefone):
+        self.nome = nome
+        self.email = email
+        self.telefone = telefone
+
+    def __str__(self):
+        return "Usuario %s (%s): %s" % (self.nome, self.email, self.telefone)
+
+
+class Cliente(Usuario):
+    def __init__(self, nome, email, telefone):
+        Usuario.__init__(self, nome, email, telefone)
+        self.animais = []
+
+    def adicionar_animal(self, animal):
+        self.animais.append(animal)
+
+    def __str__(self):
+        return "Cliente %s (%s): %s" % (self.nome, self.email, self.telefone)
+
+
+def test_usuario():
+    usuario = Usuario("Icaro", "icaro@aeroli.to", "51 81963836")
+    print usuario
+    cliente = Cliente("Igor", "igor@aeroli.to", "51 82399192")
+    print cliente
+
+    print cliente.animais
+    cliente.adicionar_animal(Animal())
+    print cliente.animais
