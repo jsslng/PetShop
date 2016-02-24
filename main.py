0f5d202ea60f9aa2d4d5b93881df8327fa8bc08b
Jessica <jessica@aeroli.to>
2016-02-24 17:30:10 -0300

    mudanças no gerenciador

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 # -*- coding: utf-8 -*-
+from animal import Animal
 
 
-# TODO: Por convenção, é melhor declarar explicitamente os imports que usar *
-from animal import test_animal, test_cachorro, test_gato, test_passaro
-from procedimento import test_procedimento
-from usuario import test_usuario
+class Gerenciador():
+    def menu(self):
+        m = True
+        animais = []
+        while m:
+            print "0: sair"
+            print "1: cadastrar um novo animal"
+            print "2: listar os animais cadastrados"
+            n = raw_input("Digite a opcao: ")
+
+            if n == "0":
+                m = False
+            elif n == "1":
+                nome = raw_input("Digite o nome do animal: ")
+                idade = raw_input("Digite a idade do animal: ")
+                raca = raw_input("Digite a raca do animal: ")
+                animal = Animal(nome, idade, raca)
+                animais.append(animal)
+            elif n == "2":
+                for c in animais:
+                    print c
 
 
-test_animal()
-test_usuario()
-test_cachorro()
-test_gato()
-test_passaro()
-test_procedimento()
+def menu_test():
+    menu = Gerenciador()
+    menu.menu()
+
+
+menu_test()
